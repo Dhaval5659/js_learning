b0d62ba9c8be9bc7f94fbbf229c46dcc83ad81cf
DhavalMangukiya <125269292+Dhaval5659@users.noreply.github.com>
2026-01-05 08:50:32 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+/*
+let score = "null"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueOfNumber = Number(score);
+console.log(typeof valueOfNumber);
+console.log(valueOfNumber);
+*/
+// "11" => 11
+// "33abc" => NAN
+// null    => 0
+// undefined => NAN
+// true => 1 , false => 0
+/*
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+// 1 => true, 0=> false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
