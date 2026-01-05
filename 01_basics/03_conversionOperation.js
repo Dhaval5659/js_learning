53575be5858658eba6f104df5b697622fbf28170
DhavalMangukiya <125269292+Dhaval5659@users.noreply.github.com>
2026-01-05 09:22:06 +0000

    conversion and comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,8 +21,30 @@ console.log(booleanIsLoggedIn);
 // 1 => true, 0=> false
 // "" => false
 // "hitesh" => true
-
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+*/
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>> Operations <<<<<<<<<<<<<<<<<<<<<<<<<
+
+let value = 3
+let negValue =-value
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = "Dhaval"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
