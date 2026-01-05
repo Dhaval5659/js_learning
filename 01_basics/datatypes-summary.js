d178bba821ffb18220198ce792537a59ebaf8320
DhavalMangukiya <125269292+Dhaval5659@users.noreply.github.com>
2026-01-05 09:53:11 +0000

    data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// primitive data types
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol,  BigInt 
+
+ 
+
+//refrence data types(non primitive)
+//Array, Objects, Functions'
+
+const heros  = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Dhaval",
+    age: 24,
+}
+console.log(myObj.name);
+
+const myFunction=function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof myFunction);
+
